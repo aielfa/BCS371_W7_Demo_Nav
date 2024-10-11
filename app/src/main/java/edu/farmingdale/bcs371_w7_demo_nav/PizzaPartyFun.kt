9589491eccce21ae7a7ca6b946ffbfcba00bda85
Francis Aiello <aielfa@farmingdale.edu>
2024-10-11 07:17:30 -0400

    Complete ToDo 9

--- app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/PizzaPartyFun.kt
+++ app/src/main/java/edu/farmingdale/bcs371_w7_demo_nav/PizzaPartyFun.kt
@@ -1,19 +1,13 @@
 package edu.farmingdale.bcs371_w7_demo_nav
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,17 +18,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.Role.Companion.RadioButton
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import kotlin.math.ceil
 
 
-// ToDo 9: make this composable navigatable and then add a button to navigate to the GPA calculator
 @Composable
 fun PizzaPartyScreen(navController: NavHostController, modifier: Modifier = Modifier) {
     var totalPizzas by remember { mutableIntStateOf(0) }
@@ -68,17 +58,26 @@ fun PizzaPartyScreen(navController: NavHostController, modifier: Modifier = Modi
             modifier = modifier.padding(top = 16.dp, bottom = 16.dp)
         )
         Button(
-            onClick = {            totalPizzas = calculateNumPizzas(numPeopleInput.toInt(),
-                hungerLevel)
+            onClick = {
 
+                if (numPeopleInput.isNotEmpty()) {
+                    totalPizzas = calculateNumPizzas(numPeopleInput.toInt(), hungerLevel)
+                }
             },
             modifier = modifier.fillMaxWidth()
         ) {
             Text("Calculate")
         }
 
+        Button(
+            onClick = { navController.navigate("gpa_calculator_screen") },
+            modifier = modifier.fillMaxWidth().padding(top = 16.dp)
+        ) {
+            Text("Go to GPA Calculator")
+        }
     }
 }
+
 
 @Composable
 fun NumberField(
